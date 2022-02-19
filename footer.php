27ba6a0dd6600f82748d08a6f99c7251ec4cf383
Ricardo Pereira Marques <ricardo80@gmail.com>
2022-02-19 11:54:15 -0300

    jquey.mask local

--- footer.php
+++ footer.php
@@ -41,7 +41,7 @@
 <script src="./plugins/select2/js/i18n/pt-BR.js"></script>
 <script src="./dist/js/priceformat/jquery.priceformat.js"></script>
 <!-- jQuery Mask Plugin  -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js" integrity="sha512-pHVGpX7F/27yZ0ISY+VVjyULApbDlD0/X0rgGbTqCE7WFW5MezNTWG/dnhtbBuICzsd0WQPgpE4REBLv+UqChw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="./dist/js/jquery.mask.min.js"></script>
 <!-- AdminLTE App -->
 <script src="./dist/js/adminlte.min.js"></script>
 <!-- AdminLTE for demo purposes -->
